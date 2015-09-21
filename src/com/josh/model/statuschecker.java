ff63fe644c3b7977997ccc56a11b1dfb06232dad
Joshua Voskamp <voskamjp@mcmaster.ca>
2015-09-21 12:07:29 -0400

    Added Project Approval Document

---
+++ src/com/josh/model/statuschecker.java
@@ -0,0 +1,16 @@
+package com.josh.model;
+
+/**
+ * Created by Josh Voskamp on 9/17/2015.
+ */
+public class statuschecker {
+    public static boolean win(){
+        if(true){
+            return false;
+        }
+        return true;
+    }
+    public static boolean validMove(){
+        return true;
+    }
+}
