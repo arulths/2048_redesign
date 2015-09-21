65d0fb2c310291bffd448a8ef5aeb9e325cec7b2
Joshua Voskamp <voskamjp@mcmaster.ca>
2015-09-21 12:07:29 -0400

    Added Project Approval Document

---
+++ src/com/josh/model/actions.java
@@ -0,0 +1,19 @@
+package com.josh.model;
+
+/**
+ * Created by Josh Voskamp on 9/17/2015.
+ */
+public class actions {
+    public boolean up(){
+        return true;
+    }
+    public boolean down(){
+        return true;
+    }
+    public boolean left(){
+        return true;
+    }
+    public boolean right(){
+        return true;
+    }
+}
---
+++ src/com/josh/view/gui.java
@@ -0,0 +1,7 @@
+package com.josh.view;
+
+/**
+ * Created by Josh Voskamp on 9/17/2015.
+ */
+public class gui {
+}
