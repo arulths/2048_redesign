9bdc8b53fef999887ca98fad367e82271364b168
Joshua Voskamp <voskamjp@mcmaster.ca>
2015-10-19 12:03:54 -0400

    Reorganize Files

---
+++ Lab_Exercises/Lab_7/Math.java
@@ -0,0 +1,8 @@
+package com.group2;
+
+public class Math {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ Lab_Exercises/Lab_8/Doxygen/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['game2048',['Game2048',['../classcom_1_1bulenkov_1_1game2048_1_1_game2048.html',1,'com::bulenkov::game2048']]]
+];
