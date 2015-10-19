5e3922f860b5b470ff6dba026ebe2ce550d9a4e6
Stephan <arulths@mcmaster.ca>
2015-10-19 12:18:44 -0400

    Upload Lab 9 exercise

---
+++ Lab_Exercises/Lab_9/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "functions.h"
+
+int main(){
+    print_hello();
+    std::cout << std::endl;
+    printf("The factorial of 5 is ");
+    factorial(5);
+    return 0;
+}
