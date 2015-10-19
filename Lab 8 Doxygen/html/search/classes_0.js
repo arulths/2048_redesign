9f159319aa1c5b25111647eb5e67d60965971c17
Joshua Voskamp <voskamjp@mcmaster.ca>
2015-10-19 12:14:26 -0400

    Remove Unorganized Files

--- Lab 8 Doxygen/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['game2048',['Game2048',['../classcom_1_1bulenkov_1_1game2048_1_1_game2048.html',1,'com::bulenkov::game2048']]]
-];
--- Math.java
+++
@@ -1,8 +0,0 @@
-package com.group2;
-
-public class Math {
-
-    public static int add(int a, int b){
-        return a + b;
-    }
-}
